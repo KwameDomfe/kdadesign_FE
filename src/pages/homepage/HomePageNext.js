e95eedb48a672e4c0f1d9e8f0dd23c64f1a11f12
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-02-16 14:10:52 +0000

    Updated Homepage Template

--- src/pages/homepage/HomePageNext.js
+++ src/pages/homepage/HomePageNext.js
@@ -10,10 +10,20 @@ const HomePageNext = () => {
                 ph2-00-m pv4-00 
                 tc"
         >
-            <h2 className="f3-00">The Next</h2>
-            <p className="tc f1-50">We bring you powerful advantages to architect your next digital transformation</p>
-             <p className="tc f1-50">We bring you powerful advantages to architect your next digital transformation</p>
-              <p className="tc f1-50">We bring you powerful advantages to architect your next digital transformation</p>
+            <h2 className="f3-00"
+            >
+                The Next
+            </h2>
+            <p className="tc f1-50">
+                We bring you powerful advantages to architect your next digital transformation.
+            </p>
+            <p className="tc f1-50">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus animi laboriosam rerum nulla inventore nostrum ullam impedit nam at reiciendis. Fdolores earum, magni maiores animi incidunt? Placeat deserunt perspiciatis sint obcaecati a..
+            </p>
+            <p className="tc f1-50"
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus animi laboriosam rerum nulla inventore nostrum ullam impedit nam at reiciendis. 
+            </p>
         </header>
         <main id="homePage_the_next--content"
             className=""
@@ -190,7 +200,7 @@ const HomePageNext = () => {
             </section>
 
             <section id="" 
-                className="grid gtc12 mv2-00 bg-gradient1BT"
+                className="grid gtc12 mv2-00"
             >
                 <div className="gc1s12 gr1s1 gc1s6-s gc1s3-m gr1s1-m"
                 >
@@ -207,7 +217,7 @@ const HomePageNext = () => {
                         </figcaption>
                         
                         <div className="flex items-center justify-center 
-                            w2-00 h2-00 white-90
+                            w2-00 h2-00 black-90
                             pb0-50 
                             f2-00 
                             ba br-50 "
@@ -230,7 +240,7 @@ const HomePageNext = () => {
                             </h3>
                         </figcaption>
                         <div className="flex items-center justify-center 
-                            w2-00 h2-00 white-90
+                            w2-00 h2-00 black-90
                             pb0-50 
                             f2-00 
                             ba br-50 "
@@ -252,7 +262,7 @@ const HomePageNext = () => {
                             </h3>
                         </figcaption>
                         <div className="flex items-center justify-center 
-                            w2-00 h2-00 white-90
+                            w2-00 h2-00 black-90
                             pb0-50 
                             f2-00 
                             ba br-50 "
@@ -274,7 +284,7 @@ const HomePageNext = () => {
                             </h3>
                         </figcaption>
                         <div className="flex items-center justify-center 
-                            w2-00 h2-00 white-90
+                            w2-00 h2-00 black-90
                             pb0-50 
                             f2-00 
                             ba br-50 "
