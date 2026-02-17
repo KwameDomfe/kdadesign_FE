89707bb8354f967f8a6923eecdd67029d244afef
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-02-17 14:32:41 +0000

    Updated the main footer

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const MainFooter = () => {
 
@@ -6,21 +7,52 @@ const MainFooter = () => {
 
     return (
         <section id=""
-            className="pv1-00 bg-black-60 vh-10"
+            className="pv1-00"
         >
-
+            <div id="m__main_nav"
+                className="flex items-center justify-center mv0-50"
+            >
+                <Link to="/"
+                    className="black-90 mh0-50"
+                >
+                    home
+                </Link>
+                <Link to="/architect-your-next"
+                    className="black-90 mh0-50"
+                >
+                    architect your next
+                </Link>
+                <Link to="/industries"
+                    className="black-90 mh0-50"
+                >
+                    industries
+                </Link>
+                <Link to="/services"
+                    className="black-90 mh0-50"
+                >
+                    services
+                </Link>
+                <Link to="/platforms"
+                    className="black-90 mh0-50"
+                >
+                    platforms
+                </Link>
+                <Link to="/info"
+                    className="black-90 mh0-50"
+                >
+                    info
+                </Link>
+            </div>
+            
             <div id="m__poweredby" 
                 className="flex flex-column items-center justify-center 
-                    white-90 h-100"
+                    black-90 h-100"
             >
-                <h2 className="f2-00 f3-00-m mt0-00 mb0-50">
-                    Main Footer
-                </h2>
 
                 <div className="flex mv0-25 items-center justify-center"
                 >
                     <a href="https://www.kdadesign.tech" 
-                        className="white-90 i"
+                        className="black-90 i"
                         target="_blank"
                         rel="noreferrer"
                     >
